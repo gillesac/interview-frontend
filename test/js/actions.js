466db07064ae197902e1d8726437e2d686ecd648
gillesac <gilles@letsface.com>
2015-04-12 01:21:52 +0200

    test finished

---
+++ test/js/actions.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function(){
+	function clearDivs() {
+		$("#breadcrumbs").remove();
+		$("#articleTitle").remove();
+		$("#articles").remove();
+		console.log("divs cleared!");
+	}
+	$.ajax({url:"articles/news.html",cache:false}).done(function(html) {
+		clearDivs();
+		$("#container1").append(html);
+	});
+	$(".menu1").click(function(){
+		$.ajax({url:"articles/news.html",cache:false}).done(function(html){
+			clearDivs();
+			$("#container1").append(html);
+		});
+	});
+	$(".menu2").click(function(){
+		$.ajax({url:"articles/business.html",cache:false}).done(function(html){
+			clearDivs();
+			$("#container1").append(html);
+		});
+	});
+});
